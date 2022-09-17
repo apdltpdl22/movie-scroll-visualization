d205dc0ec0b26a49e71ca6a4de27d1966bb108e0
apdltpdl22 <apdltpdl22@naver.com>
2022-09-17 10:47:18 +0900

    d3_01 ver

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-import { scroller } from "./scroller.js";
-import { LineChart } from "./charts/line_chart.js";
-import {
-  getMovieData
-} from './services/movie_data.js';
-
-const WIDTH = 1000;
-const HEIGHT = 1000;
 async function getData() {
   const dateSet = new Set();
   const csv = await d3.csv(`./data/movie2019.csv`);
@@ -18,32 +10,48 @@ async function getData() {
   }).sort((a, b) => a.date - b.date);
   const dates = Array.from(dateSet).sort();
 
+  return {data, dates}
+}
 
-let lineChart;
+async function makeChart() {
+  const width = 1000; 
+  const height = 1000;
+  const margin = { top: 80, right: 30, bottom: 100, left: 80 };
 
-setTimeout(drawInitial(), 100);
-
-/*
-  drawInitial 함수
-    : 초기설정을 처리합니다. 
-    1) visbox 너비와 높이 설정
-    2) 차트 클래스의 인스턴스 생성
-*/
-function drawInitial() {
-  d3.select("#visbox")
-    .append("svg")
-    .attr("width", WIDTH)
-    .attr("height", HEIGHT)
-    .attr("opacity", 1);
-
-  lineChart = new LineChart('line-chart', WIDTH, HEIGHT)
-
-  draw1();
+  const { data, dates } = await getData();
 
   const body = d3.select("body");
   const svg = body.append("svg")
       .attr("width", width)
       .attr("height", height);
+
+  // 아래 코드를 완성해보세요
+
+  // 1. x 척도 구현하기
+  //  - 시간척도(UTC)를 사용
+  //  - domain : d3.extent(dates)
+  //  - range : [margin.left, width - margin.right] 
+  const xScale =
+
+
+  // 1. y 척도 구현하기
+  //  - 선형척도를 사용
+  //  - domain : [0, d3.max(data, d => d.value)]
+  //  - range : [height - margin.bottom, margin.top] 
+  const yScale =
+
+
+  // 2. x 축 구현하기
+  //  - 아래(Bottom)에 위치
+  //  - xScale (x척도)
+  const xAxis = 
+
+
+  // 2. y 축 구현하기
+  //  - 왼쪽(Left)에 위치
+  //  - yScale (y척도)
+  const yAxis =
+
 
   svg.append('g')
     .attr('transform', 'translate(0,' + (height - margin.bottom) + ')')
@@ -69,80 +77,8 @@ function drawInitial() {
   //    - fill 속성 : 'none'
   //    - stroke 속성 : d3.schemeTableau10[0]
   svg.append()
+
+    
 }
 
-
-/*
-  clean 함수
-    : 'visType'에 따라, 해당 화면에 보여줄 필요가 없는 시각화들을 (보이지 않도록) 가립니다.
-    ex) unshow() 메소드를 호출하면, .attr("opacity", 0); 를 처리함 
-*/
-function clean(visType) {
-  const svg = d3.select("#visbox").select("svg");
-
-  if (visType !== "isFirst") {
-    svg.selectAll(".movie-img").attr("opacity", 0);
-    console.log("clean first chart");
-  }
-  if (visType !== "isLine"){
-    lineChart.unshow();
-    console.log("clean line chart");
-  }
-}
-
-
-/*
-  draw1 ~ draw3
-    : visbox(우측 화면)에 보여지는 부분 
-    - draw1 : 사진 
-    - draw2~3 : 라인차트 
- */
-function draw1() {
-  clean("isFirst");
-
-  const svg = d3.select("#visbox").select("svg");
-
-  if (!svg.select(".movie-img").empty()) {
-    svg.select(".movie-img").attr("opacity", 1);
-    return;
-  }
-
-  svg
-    .append("svg:image")
-    .attr("class", "movie-img")
-    .attr("xlink:href", "images/movie.png")
-    .attr("x", 0)
-    .attr("y", 0)
-    .attr("width", "100%")
-    .attr("height", "92%");
-}
-
-
-async function draw2() {
-  clean("isLine");
-
-  const { dates, data } = await getMovieData('2019');
-  lineChart.drawChart(dates, data).show();
-}
-
-
-async function draw3() {
-  clean("isLine");
-
-  const { dates, data } = await getMovieData('2020');
-  lineChart.drawChart(dates, data).show();
-}
-
-
-/*
-  scroller에서 
-  visFuncList에 등록된 순서대로 
-  visbox sections에 맞춰 보여줌
-*/
-const visFuncList = [
-  draw1,
-  draw2,
-  draw3,
-];
-scroller(visFuncList)();
-}
+makeChart();
